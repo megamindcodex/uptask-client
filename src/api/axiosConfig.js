ada51c3d03e417ec63f5809eaadcaac92890536c
megamindcodex <megamindcodex@gmail.com>
2025-11-26 22:18:35 +0100

    changed the axios api timeout

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -3,7 +3,7 @@ import axios from "axios";
 // Create Axios Instance
 const apiClient = axios.create({
     baseURL: import.meta.env.VITE_ENDPOINT_URL, //base url, vuejs way of using .env
-    timeout: 10000, // timeout in milliseconds
+    timeout: 20000, // timeout in milliseconds
     headers: {
         "Content-Type": "application/json"
     },
